d778ce9677834e31480458c8195ab44902bbc031
soumyo001 <www.soumyo@gmail.com>
2026-02-19 01:34:36 +0600

    reinitx2

--- components/signUpForm.tsx
+++ components/signUpForm.tsx
@@ -1,1 +1,90 @@
 "use client";
+import { useForm } from "react-hook-form";
+import { useSignUp } from "@clerk/nextjs";
+import { signUpSchema } from "@/lib/schemas/signUpSchema";
+import { z } from "zod";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { LoginForm } from "@/app/components/login-form";
+
+export default function signUpForm() {
+    const [verifying, setVerifying] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [verificationCode, setVerificationCode] = useState<string>("");
+    const [verificationError, setVerificationError] = useState<string|null>(null);
+    const [authError, setAuthError] = useState<string|null>(null);
+    const {signUp, isLoaded, setActive} = useSignUp();
+    const router = useRouter();
+
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<z.infer<typeof signUpSchema>>({
+      resolver: zodResolver(signUpSchema),
+      defaultValues: {
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      },
+    });
+    const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+        if(!isLoaded) return;
+        setIsSubmitting(true);
+        setAuthError(null);
+
+        try {
+            await signUp.create({
+                emailAddress: data.email,
+                password: data.password
+            });
+            await signUp.prepareEmailAddressVerification({
+                strategy: "email_code"
+            });
+            setVerifying(true);
+        } catch (error: any) {
+            console.log("Signup error: ", error);
+            setAuthError(
+                error.errors?.[0]?.message
+            );
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
+    const handleVerificationSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(!isLoaded || !signUp) return;
+        setIsSubmitting(true);
+        setAuthError(null);
+
+        try {
+            const result = await signUp.attemptEmailAddressVerification({
+                code: verificationCode
+            });
+            if(result.status === "complete") {
+                await setActive({session: result.createdSessionId});
+                router.push("/dashboard");
+            } else {
+                setVerificationError("Verification incomplete");
+            }
+        } catch(error: any) {
+            setVerificationError(error.errors?.[0]?.message);
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
+
+    if(verifying) {
+        return (
+            <h1>this is OTP entering field</h1>
+        )
+    }
+    return (
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm md:max-w-4xl">
+          <LoginForm />
+        </div>
+      </div>
+    );
+}
