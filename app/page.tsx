b75453f6a7e126722a1354a893bf930744fe229b
soumyo001 <www.soumyo@gmail.com>
2026-02-21 18:00:04 +0600

    clerk, react-hook-form, zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
-import SignUpForm from "./components/signUpForm";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-      <SignUpForm/>
-  );
+  redirect("/login");
 }
